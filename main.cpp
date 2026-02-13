0696272ba3f9ead4b36146dd23fd688ab0dbe16c
Joakim <superapa.spanx@gmail.com>
2026-02-13 13:41:24 +0100

    2

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include "raylib.h"
 
-int main(void) {
-    InitWindow(640, 480, "Hello Raylib!");
-    SetTargetFPS(60);
+int main() {
+    InitWindow(800, 600, "Test Pi 5 Desktop");  // Skapa fönster
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello Raylib!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Hej Pi 5 Desktop!", 200, 300, 20, LIGHTGRAY);
         EndDrawing();
     }
 
